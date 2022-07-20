0cdd8d7de3efa2bd5f7eb1555f0fed8829258014
Kim <108006977+aaaKiiM@users.noreply.github.com>
2022-07-20 19:09:39 +0700

    Perbaikan Method Logout Dan Login

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
